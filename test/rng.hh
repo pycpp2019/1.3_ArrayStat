b065fdc515b38c684c4976eaf344997a8da18aec
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-10-21 13:41:24 +0700

    Fix bugs, complete test for VecArrayStat

--- test/rng.hh
+++ test/rng.hh
@@ -11,9 +11,11 @@
 class Rng {
 private:
     uint32_t state;
+    double last_normal;
+    bool last_normal_stored;
 
 public:
-    Rng(uint32_t seed) : state(seed) {}
+    Rng(uint32_t seed) : state(seed), last_normal_stored(false) {}
 
     uint32_t rand_int() {
         return (this->state = 1103515245*this->state + 12345);
@@ -23,10 +25,17 @@ public:
         return double(this->rand_int())/0x100000000;
     }
 
-    std::pair<double, double> normal() {
-        double phi = 2.0*M_PI*this->uniform();
-        double r = -log(0.5*(1.0 - this->uniform()));
-        return std::make_pair(r*cos(phi), r*sin(phi));
+    double normal() {
+        if (this->last_normal_stored) {
+            this->last_normal_stored = false;
+            return this->last_normal;
+        } else {
+            double phi = 2.0*M_PI*this->uniform();
+            double r = -log(0.5*(1.0 - this->uniform()));
+            this->last_normal = r*sin(phi);
+            this->last_normal_stored = true;
+            return r*cos(phi);
+        }
     }
 };
 
