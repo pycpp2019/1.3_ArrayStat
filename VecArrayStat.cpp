93c38da11ae3b48b0696d46fe1952e02f2626acb
KarinaSaraeava <karinasarava@gmail.com>
2019-11-04 22:49:19 +0700

    changed

--- VecArrayStat.cpp
+++ VecArrayStat.cpp
@@ -9,13 +9,11 @@ using namespace std;
 
 double ArrayStat::rms()const{
 
-try{if (Array.size()<2) throw(1);
+if (Array.size()<2) throw(1);
     else {
         double rms = 0.0;
         std::for_each (Array.begin(), Array.end(), [&](int n)->double {rms += (n - mean()) * (n - mean());});
         return sqrt((rms)/Array.size());
-    }}catch(int y) {
-        cout<<" there are not enough elements "<< endl;
     }
 }
 
@@ -23,23 +21,18 @@ try{if (Array.size()<2) throw(1);
 
 double ArrayStat::mean() const {
 
-try{if (Array.size()<1) throw(1);
+if (Array.size()<1) throw(1);
     else {
         double sum = std::accumulate(Array.begin(), Array.end(), 0);
         return (sum/(Array.size()));
-    }}catch(int y) {
-        cout<<" there are not enough elements "<< endl;
     }
 }
 
 double ArrayStat::max() const{
 
-try{if (Array.size()<1) throw(1);
+if (Array.size()<1) throw(1);
     else
         return Array[Array.size()-1];
-    }catch (int y) {
-        cout << " there are not enough elements "<< endl;
-    }
 }
 
 size_t ArrayStat::countLarger(double a) const{
@@ -51,19 +44,17 @@ size_t ArrayStat::countLarger(double a) const{
 
 double ArrayStat::min() const{
 
-try{if (Array.size()<1) throw(1);
+if (Array.size()<1) throw(1);
     else
         return Array[0];
-    }catch (int y) {
-        cout << " there are not enough elements "<< endl;
-    }
 }
+
 
 
 ArrayStat::ArrayStat(const char* filename){
 
 ifstream  ifile(filename, ifstream::in);
-try { if (!ifile.good()){
+if (!ifile.good()){
     throw 1;
 }
 
@@ -74,24 +65,14 @@ int o;
 for (int i=0; i < k; i++) {
     o=0;
     for(int j=0; j<3; j++){
-            ifile >> N;
-            o+=N*N;
+        ifile >> N;
+        o+=N*N;
     }
 
     Array.push_back(sqrt(o));
-
-    if ((i>0) & (Array[i-1]>Array[i])) {
-        throw 2;
     }
-    }
+std::sort(Array.begin(),Array.end());
 
-}catch (int y) {
-        if (y==1){
-            cerr << "Can't open file" << endl;
-        } else {
-            cout<<"Array isn't sorted"<<endl;
-            }
-}
 }
 
 
@@ -104,7 +85,7 @@ for (int i=0; i < (Array.size()); i++) {
 
 /*int main(){
 
-    ArrayStat wow("D:/ArrayStat.txt.txt");
+    ArrayStat wow("D:/VecArrayStat.txt.txt");
     wow.print();
     cout << "max is" << wow.max() << endl;
     cout <<wow.mean()<<"rms is"<< wow.rms()<< endl;
