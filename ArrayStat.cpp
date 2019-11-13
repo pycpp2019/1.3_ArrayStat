dcb2753300baa37178aa5bb563d412fb28a21869
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-11-13 07:43:52 +0700

    Update ArrayStat.cpp

--- ArrayStat.cpp
+++ ArrayStat.cpp
@@ -8,73 +8,68 @@
 using namespace std;
 
 double ArrayStat::rms()const{
-
-if (Array.size()<2) throw(1);
+    if (Array.size()<2)
+        throw(1);
     else {
-            double m = mean();
+        double m = mean();
         double rms = 0.0;
         std::for_each (Array.begin(), Array.end(), [&](int n)->double {rms += (n - m) * (n - m);});
         return sqrt((rms)/(Array.size()-1));
     }
 }
-
-
 double ArrayStat::mean() const {
-
-if (Array.size()<1) throw(1);
+    if (Array.size()<1)
+        throw(1);
     else {
         double sum = std::accumulate(Array.begin(), Array.end(),(double) 0);
         return (sum/(double)(Array.size()));
     }}
 
 int ArrayStat::max() const{
-
-if (Array.size()<1) throw(1);
+    if (Array.size()<1)
+        throw(1);
     else
         return Array[Array.size()-1];
 }
 
 size_t ArrayStat::countLarger(int a) const{
-        int k = 0;
-  k =Array.end()- lower_bound(Array.begin(),Array.end(), (a+1)) ;   /* std::for_each (Array.begin(), Array.end(), [&](int n)->int {if (n>a)  k += 1;});*/
-return k;
+    int k = 0;
+    k =Array.end()- lower_bound(Array.begin(),Array.end(), (a+1)) ;   /* std::for_each (Array.begin(), Array.end(), [&](int n)->int {if (n>a)  k += 1;});*/
+    return k;
 }
 
 
 int ArrayStat::min() const{
-
-if (Array.size()<1) throw(1);
+    if (Array.size()<1)
+        throw(1);
     else
         return Array[0];
     }
 
 
 ArrayStat::ArrayStat(const char* filename){
+    ifstream  ifile(filename, ifstream::in);
+    if (!ifile.good()){
+        throw 1;
+    }
 
-ifstream  ifile(filename, ifstream::in);
-if (!ifile.good()){
-    throw 1;
-}
-
-int N;
-ifile >> N;
-int k = N;
-
-for (long int i=0; i < k; i++) {
+    int N;
     ifile >> N;
-    Array.push_back(N);
+    int k = N;
 
-
-
-}std::sort(Array.begin(),Array.end());}
+    for (long int i=0; i < k; i++) {
+        ifile >> N;
+        Array.push_back(N);
+    }
+    
+    std::sort(Array.begin(),Array.end());
+}
 
 
 void ArrayStat::print() const{
-
-for (int i=0; i < (Array.size()); i++) {
-    cout << Array[i] <<" "<< endl;
-
-}
+    for (int i=0; i < (Array.size()); i++) {
+        cout << Array[i] <<" "<< endl;
+    }
 }
 
 /*int main(){
@@ -85,6 +80,6 @@ for (int i=0; i < (Array.size()); i++) {
     cout << "max is" << wow.max() << endl;
     cout <<wow.mean()<<"rms is"<< wow.rms()<< endl;
 
-wow.countLarger(5);
+    wow.countLarger(5);
     return 0;
 }*/
