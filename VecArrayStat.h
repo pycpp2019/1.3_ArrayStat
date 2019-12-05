1030584f34c2c2bb5ff42e8dae60ca5f1b292f45
well120 <n.ushakova1@g.nsu.ru>
2019-12-05 19:21:12 +0700

    both

--- VecArrayStat.h
+++ VecArrayStat.h
@@ -1,16 +1,20 @@
 #include <cstdlib>
+#include "fstream"
+#include <stdexcept>
+#include <vector>
 
+using namespace std;
 
 class ArrayStat {
+    int n;
+    vector<vector<double> > dat;
+    vector<double> bdat;
 public:
     ArrayStat(const char *file_name);
-
     double max() const;
     double min() const;
-
     double mean() const;
     double rms() const;
-
     size_t countLarger(double a) const;
     void print() const;
 };
