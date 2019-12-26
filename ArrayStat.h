38239726f8bf5eb9eca87d210adb03e6e0420282
Solovev-Sergey <55825616+Solovev-Sergey@users.noreply.github.com>
2019-12-26 12:20:30 +0700

    Update ArrayStat.h

--- ArrayStat.h
+++ ArrayStat.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <cstdlib>
-
+#include <vector>
 
 class ArrayStat {
+private:
+	std::vector<int> myset;
 public:
     ArrayStat(const char *file_name);
 
@@ -13,6 +15,6 @@ public:
     double mean() const;
     double rms() const;
 
-    size_t countLarger(int a) const;
+    size_t countLarger(int key) const;
     void print() const;
 };
